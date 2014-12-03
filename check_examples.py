68f523cdd4fabfcf29e05131a85feea794df9789
Markus Demleitner <msdemlei@ari.uni-heidelberg.de>
2014-12-03 17:26:16 +0000

    RegTAP: fixed one SQL typo, ported check_examples to the ivoatex version.

--- check_examples.py
+++ check_examples.py
@@ -2,6 +2,10 @@
 Run all examples given in RegTAP.html and shout if any
 of them raises an error or returns no records.
 
+This assumes that the examples to run are in lstlisting environments
+that are immediately preceded by a CHECK_HERE comment.  Everything
+is 
+
 This requires the GAVO VOTable package (http://soft.g-vo.org).
 
 Set the TAP_ACCESS_URL environment variable to a TAP server implementing
@@ -9,8 +13,8 @@ the relational registry.
 """
 
 import os
+import re
 
-from gavo.utils import StartEndHandler
 from gavo import votable
 from gavo.votable import tapquery
 
@@ -18,31 +22,45 @@ from gavo.votable import tapquery
 TAP_ACCESS_URL = os.environ.get("TAP_ACCESS_URL", "http://dc.g-vo.org/tap")
 
 
-class ExampleChecker(StartEndHandler):
+def iter_examples(f):
+	accumulator, state = [], "scanning"
+	cur_subsection = None
 
-	lastDT = None
+	for ct, ln in enumerate(f.readlines()):
+		if state=="scanning":
+			if ln.startswith("%CHECK_HERE"):
+				state = "skipping"
+			elif ln.startswith("\\subsection"):
+				cur_subsection = re.search(r"\{([^}]*)\}", ln).group(1)
 
-	def _end_pre(self, name, attrs, content):
-		if attrs.get("class")!="samplequery":
-			return
-		try:
-			job = votable.ADQLSyncJob(TAP_ACCESS_URL, content)
-			data, metadata = votable.load(job.run().openResult())
-			if not data:
-				print "(Example returned no records: '%s')"%self.lastDT
-		except tapquery.WrongStatus:
-			print "************ Example went bad"
-			print "Last title:", self.lastDT
-			print "Error message:", job.getErrorFromServer()[:1000]
-	
-	def _start_div(self, name, attrs):
-		if attrs.get("class")=="query-example":
-			self.lastDT = attrs["id"]
+		elif state=="skipping":
+			if ln.startswith("\\begin{lstlisting}"):
+				state = "accumulating"
+			else:
+				raise Exception("Line %d: Spurious CHECK_HERE"%ct)
+
+		elif state=="accumulating":
+			if ln.startswith("\end{lstlisting}"):
+				state = "scanning"
+				yield cur_subsection, "".join(accumulator)
+				accumulator = []
+			else:
+				accumulator.append(ln)
 
 
 def main():
-	with open("RegTAP.html") as f:
-		ExampleChecker().parse(f)
+	with open("RegTAP.tex") as f:
+		for title, ex in iter_examples(f):
+			try:
+				job = votable.ADQLSyncJob(TAP_ACCESS_URL, ex)
+				data, metadata = votable.load(job.run().openResult())
+				if not data:
+					print "(Example returned no records: '%s')"%title
+			except tapquery.WrongStatus:
+				print "************ Example went bad"
+				print "Last title:", title
+				print "Error message:", job.getErrorFromServer()[:1000]
+	
 
 if __name__=="__main__":
 	main()
